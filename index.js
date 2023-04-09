287bba0aa315ff7b92ec06650075fe2e235420f9
RedstoneRadiant <76220359+TheRedstoneRadiant@users.noreply.github.com>
2023-04-09 02:35:39 +0000

    Reset Repl IP when failing to connect to MongoDB

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { exec } = require("child_process");
+
 // Load .env
 const dotenv = require("dotenv");
 dotenv.config();
@@ -38,12 +40,12 @@ const usersCollection = client.db(process.env["DATABASE"]).collection("users");
 const placedCollection = client2.db(process.env["DATABASE"]).collection("placed");
 
 const allowedUsers = [
-  "Eesa Zahed"
 ]
 
 client.connect(async (err) => {
   if (err) {
     console.log(err);
+    exec("kill 1");
     process.exit(1);
   }
 
@@ -79,7 +81,7 @@ const verifyToken = async (idToken) => {
   // get user payload
   const payload = ticket.getPayload();
 
-  if (payload.hd !== "virtuallearning.ca" && payload.hd !== "tldsb.on.ca") {
+  if (!["mustafaraza0206@gmail.com"].includes(payload.email) && (payload.hd !== "virtuallearning.ca" && payload.hd !== "tldsb.on.ca")) {
     throw "You must sign in with your VLC (@virtuallearning.ca or @tldsb.on.ca) account.";
   } else if (payload.aud !== process.env["GOOGLE_CLIENT_ID"]) {
     throw "Invalid Client ID: " + payload.aud;
@@ -89,6 +91,9 @@ const verifyToken = async (idToken) => {
 };
 
 app.get("/", (req, res) => {
+  if (!pixelArray) {
+      res.send("Oops! Failed to connect to MongoDB.");
+  }
   res.render("board", { googleClientId: process.env["GOOGLE_CLIENT_ID"], canvasHeight: pixelArray.length * 10, canvasWidth: pixelArray[0].length * 10 });
 });
 
@@ -120,67 +125,81 @@ app.post("/", async (req, res) => {
   res.send({ cooldown: cooldown });
 });
 
-app.post("/placepixel", async (req, res) => {
-  let userPayload;
+// app.post("/placepixel", async (req, res) => {
+//   let userPayload;
 
-  try {
-    userPayload = await verifyToken(req.body.token);
-  } catch (err) {
-    return res.status(405).send(err);
-  }
+//   try {
+//     userPayload = await verifyToken(req.body.token);
+//   } catch (err) {
+//     return res.status(405).send(err);
+//   }
 
-  const user = await usersCollection.findOne({ _id: userPayload.sub });
-  let cooldown;
+//   const user = await usersCollection.findOne({ _id: userPayload.sub });
+//   let cooldown;
 
-  if (user) {
-    cooldown = user.cooldown;
-  } else {
-    return res.status(405).send("Not a registered user!");
-  }
+//   if (user) {
+//     cooldown = user.cooldown;
+//   } else {
+//     return res.status(405).send("Not a registered user!");
+//   }
 
-  if (cooldown < Date.now()) {
-    try {
-      pixelArray[req.body.selectedY][req.body.selectedX] = parseInt(
-        req.body.selectedColor,
-        10
-      );
-    } catch (err) {
-      return res.sendStatus(403);
-    }
+//   if (cooldown < Date.now()) {
+//     try {
+//       pixelArray[req.body.selectedY][req.body.selectedX] = parseInt(
+//         req.body.selectedColor,
+//         10
+//       );
+//     } catch (err) {
+//       return res.sendStatus(403);
+//     }
 
-    io.emit("pixelUpdate", {
-      x: req.body.selectedX,
-      y: req.body.selectedY,
-      color: req.body.selectedColor,
-      pixelArray: pixelArray,
-      u: user._id,
-    });
+//     io.emit("pixelUpdate", {
+//       x: req.body.selectedX,
+//       y: req.body.selectedY,
+//       color: req.body.selectedColor,
+//       pixelArray: pixelArray,
+//       u: user._id,
+//     });
 
-    const cooldown = allowedUsers.includes(user.name) ? 10 : Date.now() + 15000;
-    res.send({ cooldown: cooldown });
+				// EESA EDITS
 
-    await usersCollection.updateOne(
-      { _id: userPayload.sub },
-      { $set: { cooldown: cooldown } }
-    );
+				// let userAllowedPixels = 0;
 
-    let _id = `${req.body.selectedX}${req.body.selectedY}`;
-    const pixel = await placedCollection.findOne({ _id });
-    if (!pixel) {
-      placedCollection.insertOne({
-        _id,
-        p: [{ c: req.body.selectedColor, u: user._id }],
-      });
-    } else {
-      placedCollection.updateOne(
-        { _id },
-        { $push: { p: { c: req.body.selectedColor, u: user._id } } }
-      );
-    }
-  } else {
-    return res.status(403).send({ cooldown: cooldown });
-  }
-});
+				// Todo: Fetch ahsens website and get current credit balance
+
+				// if payed {
+				// 		 userAllowedPixels = 4
+				// }
+
+				// ... NVM, todo later ;-;
+
+				// EESA EDITS
+
+//     const cooldown = allowedUsers.includes(user.name) ? 10 : Date.now() + 8000;
+//     res.send({ cooldown: cooldown });
+
+//     await usersCollection.updateOne(
+//       { _id: userPayload.sub },
+//       { $set: { cooldown: cooldown } }
+//     );
+
+//     let _id = `${req.body.selectedX}${req.body.selectedY}`;
+//     const pixel = await placedCollection.findOne({ _id });
+//     if (!pixel) {
+//       placedCollection.insertOne({
+//         _id,
+//         p: [{ c: req.body.selectedColor, u: user._id }],
+//       });
+//     } else {
+//       placedCollection.updateOne(
+//         { _id },
+//         { $push: { p: { c: req.body.selectedColor, u: user._id } } }
+//       );
+//     }
+//   } else {
+//     return res.status(403).send({ cooldown: cooldown });
+//   }
+// });
 
 app.get("/about", (req, res) => {
   res.redirect("https://en.wikipedia.org/wiki/R/place");
@@ -216,11 +235,12 @@ const sendPixelArray = (socket) => {
 
 io.on("connection", sendPixelArray);
 
-setInterval(() => {
-  if (pixelArray) {
-    boardCollection.updateOne({ _id: "latestBoard" }, { $set: { pixelArray } });
-  }
-}, 5000);
+
+// setInterval(() => {
+//   if (pixelArray) {
+//     boardCollection.updateOne({ _id: "latestBoard" }, { $set: { pixelArray } });
+//   }
+// }, 5000);
 
 server.listen(8080, () => {
   console.log("Listening on port 8080\nhttp://localhost:8080");
